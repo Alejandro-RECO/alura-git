da1b679470155ca0ed428d8b103e9168fee3fe18
Alejandro-RECO <revolledocoronado@hotmail.com>
2023-07-05 21:50:27 -0500

    Finalizando clase03

--- 1821-manipulando-dom-con-js-proyecto_inicial/scrip.js
+++ 1821-manipulando-dom-con-js-proyecto_inicial/scrip.js
@@ -5,7 +5,20 @@ const btn=document.querySelector("[data-form-btn]");
 const createTask = (evento)=>{
     evento.preventDefault();
     const input = document.querySelector("[data-form-input]");
-    console.log(input.value);
+    const value = input.value;
+    const list = document.querySelector('[data-list]');
+    const task = document.createElement('li');
+    const contenedor = `
+    <div>
+      <i class="far fa-check-square icon"></i>
+      <span class="task">${value}</span>
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    task.innerHTML = contenedor;
+    input.value = "";
+    task.classList.add("card");
+    list.appendChild(task);
+    console.log(contenedor);
 }
 
 btn.addEventListener("click", createTask)
